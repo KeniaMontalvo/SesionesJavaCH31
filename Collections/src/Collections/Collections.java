d935b1e3599c6236a62e55cf830e03ca314e9bde
Kenia Montalvo <kenlimonra@gmail.com>
2023-10-05 17:10:48 -0600

    se empezó a ver el tema de colecciones con arrays

---
+++ Collections/src/Collections/Collections.java
@@ -0,0 +1,39 @@
+package Collections;
+
+public class Collections {
+	
+	public static void main(String[] args) {
+		
+		//Array
+		//Tipo de dato del array, nombre, instancia del objeto arreglo y los datos.
+		String[] listaDeCompras = new String[] {"Leche", "Pan", "Huevos"};
+		
+		System.out.println(listaDeCompras);
+		
+		
+		
+	}
+}
+	
+	//Collections
+	
+	/*
+	 Colecciones
+	 
+	 - Arrays (arreglos)
+	 
+	 Es una estructura de datos lineal que contiene elementos del mismo tipo. Los elementos están almacenados de forma continua (uno al lado del otro) y se pueden acceder a ellos a través de un índice.
+	 
+	 Las principales características de un array son:
+	 
+	- Es una estructura de datos fija, generalmente almacena datos del mismo tipo.
+	- La longitud de un array se establece al momento de crearlo y no cambia.
+	- Podemos acceder a elementos por medio de un índice.
+	- Los arrays pueden contener datos primitivos y objetos.
+	- No proporciona métodos adicionales para agregar o eliminar elementos. 
+	
+	
+	Un ejemplo de array es una lista de compras del supermercado. 
+	 */
+
+
